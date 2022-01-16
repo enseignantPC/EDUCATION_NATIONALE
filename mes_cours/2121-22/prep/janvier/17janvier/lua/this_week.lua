ef62e570150cac66c4a4d5a973d04ac2aa3a28e8
Astrale <jules180798@yahoo.fr>
2022-01-16 11:15:29 +0100

    rebuild, nouveau plan de classe + prep-template modified

---
+++ mes_cours/2121-22/prep/janvier/17janvier/lua/this_week.lua
@@ -0,0 +1,136 @@
+-- week data
+Seance = Seance or error("Seance dependancy not satified for this_week.lua script")
+return {
+    _6A = Seance:new(
+        "6A",
+        14,
+        "act1 + question orales", -- last time
+        "", -- for today
+        "", -- next_time
+        "", --punition
+        "", -- remarque
+        "", -- creneau retenue
+        "15h55", -- horaire
+        "corriger Q23 act1 + cours à distribuer + act2" -- prgramme_du_jour
+    ),
+    _6B = Seance:new(
+        "6B",
+        23,
+        "", -- last time
+        "", -- for today
+        "", -- next_time
+        "", --punition
+        "", -- remarque
+        "", -- creneau retenue
+        "11h25", -- horaire
+        "corriger 3 1er item act1Q2 + act1 + cours + act2" -- prgramme_du_jour
+    ),
+    _6D = Seance:new(
+        "6D",
+        24,
+        "chap2act1", -- last time
+        "faireQ2-3", -- for today
+        "", -- next_time
+        "", --punition
+        "", -- remarque
+        "", -- creneau retenue
+        "14h55", -- horaire
+        "corriger act1 + cours + act2" -- prgramme_du_jour
+    ),
+    _6E = Seance:new(
+        "6E",
+        24,
+        "", -- last time
+        "", -- for today
+        "", -- next_time
+        "", --punition
+        "", -- remarque
+        "", -- creneau retenue
+        "14h55", -- horaire
+        "réopndre complot + continuer act1 + cours" -- prgramme_du_jour
+    ),
+    _6G = Seance:new(
+        "6G",
+        22,
+        "act1chap2 commencée", -- last time
+        "faire act1Q3", -- for today
+        "", -- next_time
+        "", --punition
+        "Comment un éclair se crée?", -- remarque
+        "", -- creneau retenue
+        "9h15", -- horaire
+        "voir RQ ;correct act1Q3 + copier cours act1 + act2" -- prgramme_du_jour
+    ),
+    _6H = Seance:new(
+        "6H",
+        24,
+        "", -- last time
+        "", -- for today
+        "", -- next_time
+        "", --punition
+        "", -- remarque
+        "", -- creneau retenue
+        "12h25", -- horaire
+        "corrigerQ1-2 act1 + Q3+ cours + act2" -- prgramme_du_jour
+    ),
+    _5C = Seance:new(
+        "5C",
+        24,
+        "recop cours-act2 + act3saufQ4", -- last time
+        "??", -- for today
+        "exo1 et 2 + eval1 et eval2 ", -- next_time
+        "ramasser Tandjigora", --punition
+        "", -- remarque
+        "sA :Lun15-17h -- sB Lun16h Mer8h", -- creneau retenue
+        "11h25", -- horaire
+        "eval1 + act3Q4 et suite" -- prgramme_du_jour
+    ),
+    _5D = Seance:new(
+        "5D",
+        24,
+        "eval1 + act3Q6 ok", -- last time
+        "", -- for today
+        "exo 3 et eval 3 ?", -- next_time
+        "", --punition
+        "", -- remarque
+        "sA Ven15h sB Mer8-10h", -- creneau retenue
+        "14h55", -- horaire
+        "eval2 + corriger exo1-2 + act3Q7~ + cours" -- prgramme_du_jour
+    ),
+    _5F = Seance:new(
+        "5F",
+        24,
+        "act2Q1 commencée", -- last time
+        "", -- for today
+        "eval2 ??? exo 1 et 2, reviser eval2", -- next_time
+        "ramasser doublé: Andreès x 2 ; Lune ; Nathan ; Adji", --punition
+        "Changer Bakary de place", -- remarque
+        "sA Jeu -- sB Vscol Jeu8h", -- creneau retenue
+        "10h15", -- horaire
+        "act2  + cours" -- prgramme_du_jour
+    ),
+    _5G = Seance:new(
+        "5G",
+        22,
+        "act2Q1a", -- last time
+        "exo act1", -- for today
+        "eval2 + exo2", -- next_time
+        "doublé : Marie-Love Dolynda Aboubacar", --punition
+        "", -- remarque
+        "Lun8h(vdc) Ven16h", -- creneau retenue
+        "12h25", -- horaire
+        "corriger exo + eval1 + act2 + cours" -- prgramme_du_jour
+    ),
+    _5H = Seance:new(
+        "5H",
+        22,
+        "", -- last time
+        "", -- for today
+        "recop cours act3 + eval3 + exo3", -- next_time
+        "Henzo", --punition
+        "", -- remarque
+        "sA Mar16h --sA et sB Ven14h", -- creneau retenue
+        "10h15", -- horaire
+        "eval2 + corriger ex2 + act3Q7~ + cours" -- prgramme_du_jour
+    )
+}
